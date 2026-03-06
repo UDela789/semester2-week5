8e28ca480f8f01e4e11965de7970a2650894830a
Delalorm Kunutsor <gmbp0709@leeds.ac.uk>
2026-03-06 11:22:45 +0000

    sdf

--- worksheet/task2/task2.c
+++ worksheet/task2/task2.c
@@ -2,8 +2,8 @@
  * Sem 2, week 5
  *
  * Portfolio submission: Hexadecimal to Decimal Converter
- * Name:
- * ID: 
+ * Name:Delalorm Kunutsor
+ * ID: gmbp0709
  */
 
 #include <stdio.h>
@@ -13,10 +13,24 @@ int main(void){
 	long decimal=0;
 	char hex[9];
 	
+	
 	printf("Enter a hexadecimal:");
+	scanf("%s", hex);
+	for(int character = 0; character < strlen(hex); character++){
+
+		if( (sizeof(hex) / sizeof(hex[0])) > 8 ){
+			printf("Error: Invalid Hexadecimal\n");
+			break;
+		}
+
+
+	}
+
+	
 
 	// if input contains invalid hex digit
 	// printf("Error: Invalid Hexadecimal\n");
+	
 	
 	// print the decimal result
 	printf("decimal:%ld\n", decimal);
