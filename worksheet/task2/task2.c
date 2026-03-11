c3b7ad5002943f3e3f65900cbd0134a91b97f009
Delalorm Kunutsor <gmbp0709@leeds.ac.uk>
2026-03-11 20:12:47 +0000

    asdf

--- worksheet/task2/task2.c
+++ worksheet/task2/task2.c
@@ -8,23 +8,105 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <math.h> 
 
 int main(void){
-	long decimal=0;
+	int decimal=0;
 	char hex[9];
 	
 	
-	printf("Enter a hexadecimal:");
+	
+	
+	printf("Enter a hexadecimal: ");
 	scanf("%s", hex);
-	for(int character = 0; character < strlen(hex); character++){
 
-		if( (sizeof(hex) / sizeof(hex[0])) > 8 ){
-			printf("Error: Invalid Hexadecimal\n");
-			break;
+	if(strlen(hex) > 8){
+
+		printf("Error: Invalid Hexadecimal\n");
+
+	}else{
+
+		//loop thru evry charecter
+		for(int i = 0; i < strlen(hex); ++i){
+
+			
+
+			int power = (int)pow(16, i);
+				
+
+				
+			// match-case series
+			switch (tolower(hex[i])) {
+				case '0':
+					decimal = decimal + (power * 0);
+					break;
+				case '1':
+					decimal = decimal + (power * 1);
+					break;
+				case '2':
+					decimal = decimal + (power * 2);
+					break;
+				case '3':
+					decimal = decimal + (power * 3);
+					break;
+				case '4':
+					decimal = decimal + (power * 4);
+					break;
+				case '5':
+					decimal = decimal + (power * 5);
+					break;
+				case '6':
+					decimal = decimal + (power * 6);
+					break;
+				case '7':
+					decimal = decimal + (power * 7);
+					break;
+				case '8':
+					decimal = decimal + (power * 8);
+					break;
+				case '9':
+					decimal = decimal + (power * 9);
+					break;
+				case 'a':
+					decimal = decimal + (power * 10);
+					break;
+				case 'b':
+					decimal = decimal + (power * 11);
+					break;
+				case 'c':
+					decimal = decimal + (power * 12);
+					break;
+				case 'd':
+					decimal = decimal + (power * 13);
+					break;
+				case 'e':
+					decimal = decimal + (power * 14);
+					break;
+				case 'f':
+					decimal = decimal + (power * 15);
+					break;
+				default:
+            		printf("Error: Invalid Hexadecimal\n");
+            		break;
+				
+				}
+
+				
+
+
+			
+
 		}
+		printf("decimal:%d\n", decimal);
+	
 
-
+		// print the decimal result
+		//printf("decimal:%ld\n", decimal);
 	}
+
+	
+	
 
 	
 
@@ -33,7 +115,7 @@ int main(void){
 	
 	
 	// print the decimal result
-	printf("decimal:%ld\n", decimal);
+	//printf("decimal:%ld\n", decimal);
 	
 	return 0;
 }
---
+++ worksheet/task2/test.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h> 
+
+
+
+int main(void){
+
+    char hex[] = "1A";
+    int decimal=0;
+    
+
+    printf("hex: '%s'\n\n", hex);
+
+    for(int i = 0; i > strlen(hex); ++i){
+        printf("iteration: %d\n", i );
+
+        int power = pow(16, i);
+        //printf("power: 16^%d, (%d)\n", i, power);
+
+        printf("hex[%d]: %c\n\n", i, hex[strlen(hex) - i]);
+        
+
+        // match-case series
+        switch (hex[i]) {
+            case '0':
+                decimal = decimal + (power * 0);
+                printf("decimal so far: %d\n\n", decimal);
+                break;
+            case '1':
+                decimal = decimal + (power * 1);
+                printf("decimal so far: %d\n\n", decimal);
+                break;
+            case '2':
+                decimal = decimal + (power * 2);
+                printf("decimal so far: %d\n\n", decimal);
+                break;
+            case '3':
+                decimal = decimal + (power * 3);
+                printf("decimal so far: %d\n\n", decimal);
+                break;
+            case '4':
+                decimal = decimal + (power * 4);
+                printf("decimal so far: %d\n\n", decimal);
+                break;
+            case '5':
+                decimal = decimal + (power * 5);
+                printf("decimal so far: %d\n\n", decimal);
+                break;
+            case '6':
+                decimal = decimal + (power * 6);
+                printf("decimal so far: %d\n\n", decimal);
+                break;
+            case '7':
+                decimal = decimal + (power * 7);
+                printf("decimal so far: %d\n\n", decimal);
+                break;
+            case '8':
+                decimal = decimal + (power * 8);
+                printf("decimal so far: %d\n\n", decimal);
+                break;
+            case '9':
+                decimal = decimal + (power * 9);
+                printf("decimal so far: %d\n\n", decimal);
+                break;
+            case 'a':
+            case 'A':
+                decimal = decimal + (power * 10);
+                printf("decimal so far: %d\n\n", decimal);
+                break;
+            case 'b':
+            case 'B':
+                decimal = decimal + (power * 11);
+                printf("decimal so far: %d\n\n", decimal);
+                break;
+            case 'c':
+            case 'C':
+                decimal = decimal + (power * 12);
+                printf("decimal so far: %d\n\n", decimal);
+                break;
+            case 'd':
+            case 'D':
+                decimal = decimal + (power * 13);
+                printf("decimal so far: %d\n\n", decimal);
+                break;
+            case 'e':
+            case 'E':
+                decimal = decimal + (power * 14);
+                printf("decimal so far: %d\n\n", decimal);
+                break;
+            case 'f':
+            case 'F':
+                decimal = decimal + (power * 15);
+                printf("decimal so far: %d\n\n", decimal);
+                break;
+            default:
+                printf("Error: Invalid Hexadecimal\n");
+            		break;
+        
+        }
+        
+
+
+    }
+    printf("decimal: %d", decimal);
+}
